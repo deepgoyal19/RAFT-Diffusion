1de4d0e87b6d48120b4b14414cffc153a629c093
Deepanshu Goyal <91832589+deepgoyal19@users.noreply.github.com>
2023-07-19 15:25:29 +0800

    Update finetune.py

--- examples/finetune.py
+++ examples/finetune.py
@@ -35,55 +35,9 @@ def main():
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
         # let's parse it to get our arguments.
-        model_args, model_args, data_args, raft_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+        finetuner_args, model_args, data_args, raft_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
-        model_args, model_args, data_args, raft_args = parser.parse_args_into_dataclasses()
-
-    finetuner_args = FinetunerArguments(
-        output_dir='/home/deepanshu/LMFlow-diffusion-main/examples/model',
-        enable_xformers_memory_efficient_attention=True,
-        gradient_accumulation_steps=1,
-        lr_scheduler='constant',
-        learning_rate=9e-6,
-        lr_warmup_steps=0,
-        max_train_steps=50,
-        gradient_checkpointing=True,
-        max_grad_norm=1,
-        tracker_project_name="text2image-fine-tune",
-        checkpointing_steps=40
-    )
-
-    model_args= ModelArguments(
-        pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5",
-        use_ema= False,
-        use_lora= True)
-
-    data_args=DatasetArguments(
-        resolution=256,
-        dataset_name='/home/deepanshu/LMFlow-diffusion-main/a',
-        train_batch_size=8
-    )
-
-    raft_args=RaftFinetunerArguments(
-        topk=8,
-        inference_batch_size=10,
-        epochs=2,
-        num_images_per_prompt=10,
-        clip_model_pretrained_or_path='ViT-L/14',
-        score_model='clip',
-        save_finetune_images=True,
-        grid=True
-    )
-
-    # inference_args= InferenceArguments(
-    #     grid= True,
-    #     num_images_per_prompt=5,
-    #     num_inference_steps= 100,
-    #     guidance_scale= 6,
-    #     save_format = 'jpg'
-    #     seed= 1232
-    # )
-
+        finetuner_args, model_args, data_args, raft_args = parser.parse_args_into_dataclasses()
 
     # Initialization
     raft_finetuner = RaftFinetuner(raft_args=raft_args, data_args=data_args, model_args=model_args, finetuner_args=finetuner_args)
