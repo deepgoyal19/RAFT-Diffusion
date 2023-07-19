ce733cf779b6aa63e5d4667696c923f0786aa2af
Hanze Dong <18676401+hendrydong@users.noreply.github.com>
2023-07-19 16:53:11 -0700

    Update diffusion_model.py

--- src/lmflow_aigen/models/diffusion_model.py
+++ src/lmflow_aigen/models/diffusion_model.py
@@ -117,22 +117,22 @@ class DiffusionModel:
             img_str += f"![img_{i}](./image_{i}.png)\n"
 
         yaml = f"""
----
-license: creativeml-openrail-m
-base_model: {base_model}
-tags:
-- stable-diffusion
-- stable-diffusion-diffusers
-- text-to-image
-- diffusers
-- lora
-inference: true
----
-    """
+            ---
+            license: creativeml-openrail-m
+            base_model: {base_model}
+            tags:
+            - stable-diffusion
+            - stable-diffusion-diffusers
+            - text-to-image
+            - diffusers
+            - lora
+            inference: true
+            ---
+                """
         model_card = f"""
-# LoRA text2image fine-tuning - {repo_id}
-These are LoRA adaption weights for {base_model}. The weights were fine-tuned on the {dataset_name} dataset. You can find some example images in the following. \n
-{img_str}
+            # LoRA text2image fine-tuning - {repo_id}
+            These are LoRA adaption weights for {base_model}. The weights were fine-tuned on the {dataset_name} dataset. You can find some example images in the following. \n
+            {img_str}
         """
         with open(os.path.join(repo_folder, "README.md"), "w") as f:
             f.write(yaml + model_card)
